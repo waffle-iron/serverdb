fb5983c7607bec1d3f4be04114e352c6749b03ef
Joseph Waggy <jwaggy@endstech.com>
2017-01-19 22:56:13 -0500

    log

--- serverdb/libfunc.py
+++ serverdb/libfunc.py
@@ -1,5 +1,5 @@
 import ipaddress, logging
-
+from logging.handlers import RotatingFileHandler
 
 def ip_is_valid(host):
     """checks for a valid host ip"""
@@ -13,3 +13,15 @@ def ip_is_valid(host):
         valid = False
         logging.warning('Ip not valid!')
         return valid
+
+def create_rotating_log(path):
+    """
+    Creates a rotating log
+    """
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.INFO)
+
+    # add a rotating handler
+    handler = RotatingFileHandler(path, maxBytes=10000000,
+                                  backupCount=2)
+    logger.addHandler(handler)
