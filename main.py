6ece527ea09db86d6a6b5e9f794ebbb83d42aafe
jwaggy <jwaggy@endstech.com>
2017-01-20 16:52:48 -0500

    further developing gui

---
+++ main.py
@@ -0,0 +1,3 @@
+import serverdb
+from serverdb import libfunc, gui, mariadbadapter
+
--- serverdb/gui.py
+++ serverdb/gui.py
@@ -1,5 +1,83 @@
-import tkinter
-from tkinter import ttk, messagebox
+import tkinter, logging
+from tkinter import ttk, messagebox, showerror
+
+def excepts_handle(arg):
+    """show's fatal box and quits"""
+    tkmessagebox.showerror("Fatal", "%s" % arg)
+    quit()
+
+def new():
+    sdf
+
+def on_quit():
+    """on quit func"""
+    temp = messagebox.askokcancel("Server History Viewer", "Are you sure you want to quit?")
+    print(temp)
+    if (temp == 1):
+        quit()
+    else:
+        pass
+def view():
+    """dialog box for lookup"""
+    def gets():
+        """retrieves data"""
+        logging.debug('getting vars')
+        cn_get = str(entry1.get()).lower()
+        pn_get = str(entry2.get()).lower()
+        sn_get = str(entry3.get()).lower()
+        logging.debug('closing window')
+        viewvar.destroy()
+        logging.debug('returning data to parent window')
+        return (cn_get, pn_get, sn_get)
+    logging.debug('building window')
+    viewvar = tkinter.Toplevel(root)
+    viewvar.title('Machine lookup')
+    label1 = tkinter.Label(viewvar, text="Company Name  ")
+    label1.grid(column=0, row=0, sticky='wns')
+    label2 = tkinter.Label(viewvar, text="Plant Name    ")
+    label2.grid(column=0, row=1, sticky='wns')
+    label3 = tkinter.Label(viewvar, text="Serial Number ")
+    label3.grid(column=0, row=2, sticky='wns')
+    entry1 = tkinter.Entry(viewvar)
+    entry1.grid(column=1, row=0, columnspan=3, sticky='ns')
+    entry2 = tkinter.Entry(viewvar)
+    entry2.grid(column=1, row=1, columnspan=3, sticky='ns')
+    entry3 = tkinter.Entry(viewvar)
+    entry3.grid(column=1, row=2, columnspan=3, sticky='ns')
+    but1 = tkinter.Button(viewvar, text="OK", command=gets)
+    but1.grid(column=2, row=3, sticky='ens')
+    for child in viewvar.winfo_children():
+        child.grid_configure(padx=5, pady=5)
+    logging.debug('returning data to main')
+    return gets
+
+def get_hostname():
+    """dialog box for hostname"""
+    try:
+        def gets():
+            """retrieves data"""
+            logging.debug('getting ip entry')
+            ip_get = str(entry1.get()).lower()
+            logging.debug('closing window')
+            hostbox.destroy()
+            logging.debug('returning data to parent window')
+            return ip_get
+        logging.debug('building window')
+        hostbox = tkinter.Toplevel(root)
+        hostbox.title('Host Name')
+        label1 = Label(hostbox, text="Enter your host name")
+        label1.grid(column=0, row=0)
+        entry1 = tkinter.Entry(hostbox)
+        entry1.grid(column=1, row=0, columnspan=2)
+        but1 = tkinter.Button(hostbox, text="OK", command=gets)
+        but1.grid(column=2, row=1, sticky='w')
+        for child in hostbox.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+        logging.debug('returning data to main')
+        return gets
+    except * as excepts:
+        logging.fatal('Something went wrong with the hostname dialog %s') % (excepts)
+        excepts_handle(excepts)
 
 class Querier(ttk.Frame):
     """The db querier gui and functions"""
@@ -22,45 +100,27 @@ class Querier(ttk.Frame):
         self.menubar = tkinter.Menu(self.root)
 
         self.menu_file = tkinter.Menu(self.menubar)
-        self.menu_file.add_command(label='Exit', command=self.on_quit)
+        self.menu_file.add_command(label='New', command=new)
+        self.menu_file.add_command(label='View', command=view)
+        self.menu_file.add_command(label='Exit', command=on_quit)
+        
 
         self.menu_edit = tkinter.Menu(self.menubar)
-        self.menu_edit.add_command(label='View', command=self.view)
+        self.menu_edit.add_command(label='Reset', command=resetvars)
+        
 
         self.menubar.add_cascade(menu=self.menu_file, label='File')
         self.menubar.add_cascade(menu=self.menu_edit, label='Edit')
 
         self.root.config(menu=self.menubar)
 
-    def on_quit():
-        """on quit func"""
-        temp = messagebox.askokcancel("Server History Viewer", "Are you sure you want to quit?")
-        print(temp)
-        if (temp == 1):
-            quit()
-        else:
-            pass
-    def view(self):
-        hfh
-
-    def get_hostname():
-        hostbox = tkinter.Toplevel(root)
-        hostbox.title('Host Name')
-        label1 = Label(hostbox, text="Enter your host name")
-        label1.grid(column=1, row=0)
-        entry1 = tkinter.Entry(hostbox)
-        entry1.grid(column=2, row=0, columnspan=2)
-        but1 = tkinter.Button(hostbox, text="OK", command=get)
-        but1.grid(column=2, row=1)
-        def getter():
-            ip = str(entry1.get())
-            return ip
-        return ip
+    
+        
 
 
 
 
 if __name__ == '__main__':
     root = tkinter.Tk()
-    querier(root)
+    Querier(root)
     root.mainloop()
